e20c07332fc7a18a7a3ba7d79e20c72506a573a5
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-01-05 14:07:45 +0100

    fix userRouter

--- module-09-rest/cleanup/src/routes/userRouter.ts
+++ module-09-rest/cleanup/src/routes/userRouter.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import { getUsers, createUser, getUserById, updateUser, deleteUser } from '#controllers';
 
 const userRouter = Router();
-userRouter.get('/users', getUsers);
-userRouter.post('/users', createUser);
-userRouter.get('/users/:id', getUserById);
-userRouter.put('/users/:id', updateUser);
-userRouter.delete('/users/:id', deleteUser);
+userRouter.get('/', getUsers);
+userRouter.post('/', createUser);
+userRouter.get('/:id', getUserById);
+userRouter.put('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
 
 export default userRouter;
