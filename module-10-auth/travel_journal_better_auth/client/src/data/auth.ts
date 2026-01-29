b7f523715a19b5e4073cc6c54578588729dd7161
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-01-29 14:38:00 +0100

    fix env bug

--- module-10-auth/travel_journal_better_auth/client/src/data/auth.ts
+++ module-10-auth/travel_journal_better_auth/client/src/data/auth.ts
@@ -2,8 +2,11 @@ import { inferAdditionalFields } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-	baseURL: import.meta.env.VITE_AUTH_SERVICE_URL,
-	trustedOrigins: [import.meta.env.VITE_APP_TRAVEL_JOURNAL_API_URL],
+	baseURL: import.meta.env.VITE_APP_AUTH_SERVICE_URL,
+	trustedOrigins: [
+		import.meta.env.VITE_APP_TRAVEL_JOURNAL_API_URL,
+		import.meta.env.VITE_APP_AUTH_SERVICE_URL,
+	],
 	plugins: [
 		inferAdditionalFields({
 			user: {
